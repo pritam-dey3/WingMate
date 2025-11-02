aa17e0bb156429166af1c40882393befea2755ec
pritam-dey3 <pdeypritam@gmail.com>
2025-11-02 09:35:26 +0530

    remove simple example

--- examples/simple.py
+++ examples/simple.py
@@ -1,16 +1,1 @@
-from typing import Union
-
-from localagent._types import Action, AgentResponse
-
-
-class Answer(Action):
-    query_resolved: bool
-
-
-class Question(Action):
-    pass
-
-
-actions = [Answer, Question]
-MyReq = AgentResponse[Union[tuple(actions)]]
-print(MyReq.model_json_schema())
+# coming soon...
